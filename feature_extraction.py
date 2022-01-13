7b3c24e9322cb64d5e2e9e226445f866882e36c5
chocobearz <ptuttosi@gmail.com>
2022-01-12 16:05:40 -0800

    make help messages more explicit

--- feature_extraction.py
+++ feature_extraction.py
@@ -6,44 +6,55 @@ import argparse
 #from spectral_functions import *
 from parsel_process import * 
 
-'''
-This file will return the ..... spectral slope, mfcc, and mean spectral roll-off
-'''
-
-# TODO consider allowing the user to input the functions to call
-parser = argparse.ArgumentParser(description = "Provide the sampling rate and file path")
-parser.add_argument(
-  "samplingRate",
-  help="desired sample rate for the file to be read with"
-)
-parser.add_argument(
-  "filePath",
-  help="path to the file (including '.wav extension), or folder containing the files, audio files must be in .wav format"
-)
-parser.add_argument(
-    "--writePath", default="spectral_features.csv",
-    help = "optional file to append result to, default is a new file in the current location"
-)
-args = parser.parse_args()
-
-SAMPLING_RATE = args.samplingRate
-PATH = args.filePath
-WRITE_PATH = args.writePath
-
 
 if __name__ =='__main__':
-    parser = argparse.ArgumentParser(description = "Provide the sampling rate and file path")
-    #parser.add_argument("--filepath", help='file path')
-    parser.add_argument("--formants", type=str, default=False, help="feature 1")
-    parser.add_argument("--ZCR", type=str, default=False, help="feature 2")
-    parser.add_argument("--harmonics", type=str, default=False, help="feature 3")
-    parser.add_argument("--rate_of_speech", type=str,  default=False, help="feature 4")
-    parser.add_argument("--loudness", type=str,  default=False, help="feature 5")
-    parser.add_argument("--pitch_features", type=str,  default=False, help="feature 6")
-    parser.add_argument("--spectral_features", type=str,  default=False, help="feature 7") 
+    parser = argparse.ArgumentParser(description = "Provide the sampling rate and file path and features to be extracted")
+    parser.add_argument(
+      "samplingRate",
+      help="desired sample rate for the file to be read with"
+    )
+    parser.add_argument(
+      "filePath",
+      help="path to the file (including '.wav extension), or folder containing the files, audio files must be in .wav format"
+    )
+    parser.add_argument(
+        "--writePath", default="spectral_features.csv",
+        help = "file to append result to, default is a new file in the current location"
+    )
+    parser.add_argument(
+      "--formants", type=str, default=False,
+      help="extract 4 formants"
+    )
+    parser.add_argument(
+      "--ZCR", type=str, default=False,
+      help="extract zero crossing rate"
+    )
+    parser.add_argument(
+      "--harmonics", type=str, default=False,
+      help="extract harmonics")
+    parser.add_argument(
+      "--rate_of_speech", type=str,  default=False,
+      help="extract number of syllables, words and pauses"
+      )
+    parser.add_argument(
+      "--loudness", type=str,  default=False,
+      help="extract max intensity and intensity"
+    )
+    parser.add_argument(
+      "--pitch_features", type=str,  default=False,
+      help="extract max pitch jump, peak to valley, pitch and pitch range"
+      )
+    parser.add_argument(
+      "--spectral_features", type=str,  default=False,
+      help="extract spectral envelope, spectral slope, mfcc and mean spectral roll off"
+    )
     parser.add_argument("--energy", type=str,  default=False, help="feature 8") 
 
     args = parser.parse_args()
+
+    SAMPLING_RATE = args.samplingRate
+    PATH = args.filePath
+    WRITE_PATH = args.writePath
     
     function_dic = {"formants": [analyse_formants(1), analyse_formants(2), analyse_formants(3), analyse_formants(4)],
                     "ZCR": [analyze_zero_crossing],
@@ -51,14 +62,14 @@ if __name__ =='__main__':
                     "rate_of_speech": [get_number_sylls, get_number_words, pauses],
                     "loudness": [get_max_intensity, analyse_intensity], 
                     "pitch_features":[max_jump, peak_to_valley, analyse_pitch, analyze_pitch_range],
-                    "spectral_features":[get_envelope, spectral_slope, analyse_mfcc, mean_spectral_rollof]}
+                    "spectral_features":[get_envelope, spectral_slope, analyse_mfcc, mean_spectral_rollof],
+                    "energy":[get_energy]}
     
     # Files are read in order of the time created
     if ".wav" in PATH:
       pathlist = [PATH]
     else :
       pathlist = sorted(Path(PATH).glob('**/*.wav'), key=os.path.getmtime)
-    pathlist = sorted(Path(PATH).glob('**/*.wav'), key=os.path.getmtime)
     dic = {}
     for k in args.__dict__:
       print(k)
